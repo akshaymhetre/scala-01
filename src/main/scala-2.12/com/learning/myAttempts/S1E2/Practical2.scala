8af11ed0ece8a31d2f6e2bd17283b413b58d98fd
akshay <akshaymh@gslab.com>
2017-03-15 16:22:27 +0530

    Basic Template for scala workshop

--- src/main/scala-2.12/com/learning/myAttempts/S1E2/Practical2.scala
+++ src/main/scala-2.12/com/learning/myAttempts/S1E2/Practical2.scala
@@ -5,7 +5,19 @@ package com.learning.myAttempts.S1E2
   */
 object SumOfNumWithShortFun {
 
-  def sumR(fun: Int => Int, a: Int, b:Int):Int = if(a > b) 0 else fun(a) + sumR(fun, a+1,b)
+  def sumR(fun: Int => Int, a: Int, b:Int):Int =
+    if(a > b) 0 else fun(a) + sumR(fun, a+1,b)
+
+  def prodR(fun: Int => Int, a: Int, b:Int):Int =
+    if(a > b) 1 else fun(a) * prodR(fun, a+1,b)
+
+  def mapReduce(a: Int, b: Int,
+                map: Int=>Int,
+                reduce: (Int, Int) => Int,
+                pivot: Int):Int = if(a> b) pivot
+                              else
+                              reduce(map(a),mapReduce(a+1,b, map, reduce, pivot))
+
 
   def sumCubes(a: Int, b:Int) =  sumR(a => a*a*a, a, b)
   def sumIds(a: Int, b: Int) = sumR(a => a, a, b)
@@ -15,6 +27,7 @@ object SumOfNumWithShortFun {
 }
 
 
-object Practical2 {
-
+object Practical2 extends App{
+  println(SumOfNumWithShortFun.mapReduce(1, 10, a=> a*a*a, (a,b) => a+b, 0))
+  println(SumOfNumWithShortFun.sumCubes(1, 10))
 }
